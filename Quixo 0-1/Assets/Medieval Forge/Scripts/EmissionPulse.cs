9908203404a42a4a267b14a64fa2a73395754eac
calebmichaelarnold <130587377+calebmichaelarnold@users.noreply.github.com>
2024-02-10 20:16:55 -0600

    added forge asset

---
+++ Quixo 0-1/Assets/Medieval Forge/Scripts/EmissionPulse.cs
@@ -0,0 +1,24 @@
+﻿	using UnityEngine;
+	using System.Collections;
+	
+	public class EmissionPulse : MonoBehaviour 
+	{
+		public float frequency = 1f;
+		public float amplitude = 1f;
+		public float baseMult = 0.75f;
+		public Material material;
+		public Color emissionColor;
+		
+		// Use this for initialization
+		void Start () 
+		{
+		emissionColor = emissionColor * baseMult * amplitude;
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			float glow = (2 + Mathf.Cos(Time.time * frequency)) * amplitude;
+			material.SetColor("_EmissionColor", emissionColor * glow);
+		}
+	}
--- Quixo 0-1/Assets/Scrpts/PieceLogic.cs
+++ Quixo 0-1/Assets/Scrpts/PieceLogic.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class PieceLogic : MonoBehaviour
 {
-    //F: Gives each spawned piece characteristics that we can use low-level
     public int row;
     public int col;
     public char player;
     public GameCore game;
-    private ButtonHandler buttonHandler;
 
     private bool isSelected = false;
-    private Rigidbody rb;
     private List<char> moveList;
-  
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     private void OnMouseDown()
     {
         if (game.validPiece(row, col))
         {
-            GetComponent<Rigidbody>().useGravity = false; //F: lifts the piece for a second
-            transform.position = new Vector3(transform.position.x, 15f, transform.position.z); // raises the selected piece
+            
+            
+            
+            game.lowerPiece(); // Calls the function from the GameCore class to lower all raised pieces
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             isSelected = true;
 
             moveList = game.moveOptions(row, col); // Creates a list of directional moves based on the piece selected
-            GetComponent<Rigidbody>().useGravity = true; //F: drops the piece back down, giving it the falling effect
         }
     }
 
