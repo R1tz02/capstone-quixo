a35adae6d1332150ce0625c3615ca449705d68b7
Fernando Vides <fvidesaguilar@harding.edu>
2024-02-14 13:12:16 -0600

    Fix bugs and made the code more efficient

---
+++ Quixo 0-1/Assets/Scrpts/ButtonHandler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//F: This script controls the arrows that display the options for the moves
+
+public class ButtonHandler : MonoBehaviour
+{
+    public Button left;
+    public GameObject what;
+    public Button right;
+    public Button up;
+    public Button down;
+    public GameCore game;
+
+
+    void Start()
+    {
+        game = GameObject.FindObjectOfType<GameCore>();
+        up.onClick.AddListener(delegate { doOnClick('u'); });   //F: Give each arrow an on click event listener that calls doOnClick and we pass it a char
+        down.onClick.AddListener(delegate { doOnClick('d'); }); // representing what arrow was clicked
+        left.onClick.AddListener(delegate { doOnClick('l'); }); 
+        right.onClick.AddListener(delegate { doOnClick('r'); });
+    }
+
+
+    private void doOnClick(char dir)
+    {
+        game.makeMove(dir); //F: make a move
+    }
+
+    public void changeArrowsBack() //F: we change the arrows back to white
+    {
+        left.GetComponent<Image>().color = Color.white;
+        right.GetComponent<Image>().color = Color.white;
+        up.GetComponent<Image>().color = Color.white;
+        down.GetComponent<Image>().color = Color.white;
+    }
+
+    public void changeArrowColor(char dir)
+    {
+        switch (dir) //F: Change corresponding arrows depending on the available move options
+        {
+            case 'U': up.GetComponent<Image>().color = Color.red; 
+                break;
+            case 'D':
+                down.GetComponent<Image>().color = Color.red;
+                break;
+            case 'L':
+                left.GetComponent<Image>().color = Color.red;
+                break;
+            case 'R':
+                right.GetComponent<Image>().color = Color.red;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { doOnClick('u'); }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { doOnClick('d'); }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { doOnClick('l'); }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { doOnClick('r'); }
+    }
+}
--- Quixo 0-1/Assets/Scrpts/GameCore.cs
+++ Quixo 0-1/Assets/Scrpts/GameCore.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using static UnityEngine.Rendering.DebugUI.Table;
 
 public class GameCore : MonoBehaviour
-{ 
+{
     public GameObject piecePrefab;
     public Material playerOneSpace;
     public Material playerTwoSpace;
+<<<<<<< Updated upstream
 
     private PieceLogic pieceLogic;
     public GameObject[,] gameBoard = new GameObject[5, 5];
@@ -17,6 +18,267 @@ public class GameCore : MonoBehaviour
     void Start()
     {
         populateBoard();
+=======
+    public ButtonHandler buttonHandler;
+    private PieceLogic pieceLogic;
+    private PieceLogic chosenPiece;
+    private GameObject[,] gameBoard = new GameObject[5, 6];
+    private Renderer rd;
+    public Player currentPlayer = new Player();
+    public Player p1 = new Player();
+    public Player p2 = new Player();
+    public int counter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1.piece = 'X'; //F: assign X to player one
+        currentPlayer = p1; //F: make X the first player/move
+        p2.piece = 'O'; //F: assign O to player two
+        buttonHandler = GameObject.FindObjectOfType<ButtonHandler>();
+        populateBoard(); //Initialize board
+    }
+
+
+    private bool horizontalWin()
+    {
+        Debug.Log("checking for horizontal win");
+        bool success;
+        char baseSymbol = '-';
+        char pieceToCheck = '-';
+        for (int row = 0; row < 5; row++)
+        {
+            success = true;
+            baseSymbol = gameBoard[row, 0].GetComponent<PieceLogic>().player; //F: first value of every row is base
+            for (int col = 0; col < 5; col++)
+            {
+                pieceToCheck = gameBoard[row, col].GetComponent<PieceLogic>().player; //F: assigned to a variable instead of callind GetComponent twice in the if
+                if (pieceToCheck != baseSymbol || pieceToCheck == '-') //F: compare every item to the baseSymbol, ignore immediately if it is blank
+                {
+                    success = false; //F: if changed, not same symbols
+                    break; //F: get out if not same symbol or blank, and try the next
+                }
+            }
+            if (success) //F: If unchanged, we have a win
+            {
+                if (p1.piece == baseSymbol)
+                {
+                    p1.won = true;
+                    currentPlayer = p1;
+                }
+                else
+                {
+                    p2.won = true;
+                    currentPlayer = p2;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool verticalWin()
+    {
+        Debug.Log("checking for vertical win");
+        bool success;
+        char baseSymbol = '-';
+        char pieceToCheck = '-';
+        for (int col = 0; col < 5; col++)
+        {
+            success = true;
+            baseSymbol = gameBoard[0, col].GetComponent<PieceLogic>().player; ;
+            for (int row = 0; row < 5; row++)
+            {
+                pieceToCheck = gameBoard[row, col].GetComponent<PieceLogic>().player;
+                if (pieceToCheck != baseSymbol || pieceToCheck == '-')
+                {
+                    success = false;
+                    break;
+                }
+            }
+
+            if (success)
+            {
+                if (p1.piece == baseSymbol)
+                {
+                    p1.won = true;
+                    currentPlayer = p1;
+                }
+                else
+                {
+                   p2.won = true;
+                   currentPlayer = p2;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool leftDiagonalWin()
+    {
+        Debug.Log("check leftdiagonal win");
+        char baseSymbol = '-';
+        char pieceToCheck = '-';
+        bool success = true;
+        //check for top left to bottom right win
+        baseSymbol = gameBoard[0, 0].GetComponent<PieceLogic>().player; ;
+        for (int i = 1; i < 5; i++)
+        {
+            pieceToCheck = gameBoard[i, i].GetComponent<PieceLogic>().player;
+            if (pieceToCheck != baseSymbol || pieceToCheck == '-')
+            {
+                success = false;
+                break;
+            }
+        }
+        if (success)
+        {
+            if (p1.piece == baseSymbol)
+            {
+                p1.won = true;
+                currentPlayer = p1;
+            }
+            else
+            {
+                p2.won = true;
+                currentPlayer = p2;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool rightDiagonalWin()
+    {
+        //check for bottom left to top right 
+        char pieceToCheck = '-';
+        char baseSymbol = gameBoard[0, 4].GetComponent<PieceLogic>().player;
+        bool success = true;
+        for (int i = 0; i < 5; i++)
+        {
+            pieceToCheck = gameBoard[i, 4 - i].GetComponent<PieceLogic>().player;
+            if (pieceToCheck != baseSymbol || pieceToCheck == '-')
+            {
+                success = false;
+                break;
+            }
+        }
+
+        if (success)
+        {
+            if (p1.piece == baseSymbol)
+            {
+                p1.won = true;
+                currentPlayer = p1;
+            }
+            else
+            {
+                p2.won = true;
+                currentPlayer = p2;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public bool won()
+    {
+        if (horizontalWin()) return true;
+        if (verticalWin()) return true;
+        if (leftDiagonalWin()) return true; //separated checkDiagonalWin into two separate functions
+        if (rightDiagonalWin()) return true;
+        return false;
+    }
+
+    private void moveChosenPiece(int row, int col, Material pieceColor, char currentPiece, float x, float y, float z)
+    {
+        gameBoard[row, col] = gameBoard[0, 5]; //F: set the selected piece to its new position in the array
+        gameBoard[row, col].GetComponent<PieceLogic>().player = currentPiece; //F: changing the moved piece's symbol to the current
+        gameBoard[row, col].GetComponent<Renderer>().material = pieceColor; //F: changing the moved piece's material (color) 
+        gameBoard[row, col].transform.position = new Vector3(x, y, z); //F: physically move the selected piece
+        gameBoard[row, col].GetComponent<PieceLogic>().row = row; //F: changing the moved piece's row
+        gameBoard[row, col].GetComponent<PieceLogic>().col = col; //F: changing the moved piece's col
+    }
+    
+    private void shiftBoard(char dir, char currentPiece)
+    {
+        Debug.Log(dir);
+        gameBoard[0,5] = gameBoard[chosenPiece.row, chosenPiece. col]; //F: [0,5] is permanently used as a temp index in which we hold the selected piece
+        Material pieceColor; //F: Made a variable to change the material of the piece depending on the currentPlayer
+        switch (currentPiece) 
+        {
+            case 'X': pieceColor = playerOneSpace;
+                break;
+            default: pieceColor = playerTwoSpace; break;
+        }
+
+        if (dir == 'u') 
+        {
+            for (int i = chosenPiece.row; i > 0; i--)
+            {
+                gameBoard[i - 1, chosenPiece.col].GetComponent<PieceLogic>().row = i; //F: Change the piece's underlying variable holding its row
+                gameBoard[i - 1, chosenPiece.col].transform.position = new Vector3(gameBoard[i - 1, chosenPiece.col].transform.position.x + 20, 5, gameBoard[i - 1, chosenPiece.col].transform.position.z); //F: Physically move every piece in the row/col
+                gameBoard[i, chosenPiece.col] = gameBoard[i - 1, chosenPiece.col]; //F: updating the array (low level implementation of the game)
+            }
+            moveChosenPiece(0, chosenPiece.col, pieceColor, currentPiece, -40, 5, gameBoard[1, chosenPiece.col].transform.position.z);
+        }
+        else if (dir == 'd')
+        {
+            for (int i = chosenPiece.row; i < 4; i++)
+            {
+                gameBoard[i + 1, chosenPiece.col].GetComponent<PieceLogic>().row = i;
+                gameBoard[i + 1, chosenPiece.col].transform.position = new Vector3(gameBoard[i + 1, chosenPiece.col].transform.position.x - 20, 5, gameBoard[i + 1, chosenPiece.col].transform.position.z);
+                gameBoard[i, chosenPiece.col] = gameBoard[i + 1, chosenPiece.col];
+            }
+            moveChosenPiece(4, chosenPiece.col, pieceColor, currentPiece, 40, 5, gameBoard[1, chosenPiece.col].transform.position.z);
+        }
+        else if (dir == 'r')
+        {
+            for (int i = chosenPiece.col; i < 4; i++)
+            {
+                gameBoard[chosenPiece.row, i + 1].GetComponent<PieceLogic>().col = i;
+                gameBoard[chosenPiece.row, i + 1].transform.position = new Vector3(gameBoard[chosenPiece.row, i + 1].transform.position.x, 5, gameBoard[chosenPiece.row, i + 1].transform.position.z - 20);
+                gameBoard[chosenPiece.row, i] = gameBoard[chosenPiece.row, i + 1];
+            }
+            moveChosenPiece(chosenPiece.row, 4, pieceColor, currentPiece, gameBoard[chosenPiece.row, 1].transform.position.x, 5, 40);
+        }
+        else if (dir == 'l')
+        {
+            for (int i = chosenPiece.col; i > 0; i--)
+            {
+                gameBoard[chosenPiece.row, i - 1].GetComponent<PieceLogic>().col = i;
+                gameBoard[chosenPiece.row, i - 1].transform.position = new Vector3(gameBoard[chosenPiece.row, i - 1].transform.position.x, 5, gameBoard[chosenPiece.row, i - 1].transform.position.z + 20);
+                gameBoard[chosenPiece.row, i] = gameBoard[chosenPiece.row, i - 1];
+            }
+            moveChosenPiece(chosenPiece.row, 0, pieceColor, currentPiece, gameBoard[chosenPiece.row, 1].transform.position.x, 5, -40);
+        }
+    }
+
+    public void makeMove(char c)
+    {
+        if (validPiece(chosenPiece.row, chosenPiece.col))
+        {
+            shiftBoard(c, currentPlayer.piece);
+            counter++;
+            Debug.Log("make move called");
+            buttonHandler.changeArrowsBack(); //F: change arrows back for every new piece selected
+            if (counter > 8 && won()) //F: TODO add counter 
+            {
+                GameObject instantiatedPrefab = Instantiate(winnerText, Vector3.zero, Quaternion.identity); //F: instantiating the winner text pop up, this is temporal
+                instantiatedPrefab.GetComponent<TextMesh>().text = currentPlayer.piece + " won!"; //F: setting its output content
+                instantiatedPrefab.SetActive(true); //F: make it appear
+                instantiatedPrefab.transform.position = new Vector3(0, 16, 50); //F: adjust it so it is visible
+                instantiatedPrefab.transform.rotation = Quaternion.Euler(90, -90, 0); //F: ^
+                Debug.Log(currentPlayer.piece + " won!");
+                return;
+            }
+            //F: TODO - work on validmove error handling
+            else if (currentPlayer.piece == 'X') { currentPlayer = p2; } else { currentPlayer = p1; }; //F: if not won, we change the currentPlayer
+        }
+>>>>>>> Stashed changes
     }
 
     //Returns a list of Directions a piece can move based on the locations of the passes piece
@@ -26,18 +288,34 @@ public class GameCore : MonoBehaviour
         if (row > 0)
         {
             moveList.Add('U');
+<<<<<<< Updated upstream
+=======
+            buttonHandler.changeArrowColor('U');
+>>>>>>> Stashed changes
         }
         if (row < 4)
         {
             moveList.Add('D');
+<<<<<<< Updated upstream
+=======
+            buttonHandler.changeArrowColor('D');
+>>>>>>> Stashed changes
         }
         if (col > 0)
         {
             moveList.Add('L');
+<<<<<<< Updated upstream
+=======
+            buttonHandler.changeArrowColor('L');
+>>>>>>> Stashed changes
         }
         if (col < 4)
         {
             moveList.Add('R');
+<<<<<<< Updated upstream
+=======
+            buttonHandler.changeArrowColor('R');
+>>>>>>> Stashed changes
         }
         return moveList;
     }
@@ -97,4 +375,8 @@ public class GameCore : MonoBehaviour
     {
         
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
