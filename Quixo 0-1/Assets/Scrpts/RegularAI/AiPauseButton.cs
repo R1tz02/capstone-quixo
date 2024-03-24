8070d0a2eebf01e70ebcc2831901056713a1e9d2
Fernando Vides <fvidesaguilar@harding.edu>
2024-03-23 20:54:58 -0500

    Initial AI Scene work

---
+++ Quixo 0-1/Assets/Scrpts/RegularAI/AiPauseButton.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics.Contracts;
+//using UnityEditor.Overlays;
+
+public class AiPauseButton : MonoBehaviour
+{
+    public Canvas pauseMenu;
+    public Canvas helpMenu;
+    public Button pauseButton;
+    public GameObject gameMaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        pauseMenu.enabled = false;
+        helpMenu.enabled = false;
+    }
+
+    public void openMenu()
+    { 
+        pauseMenu.enabled = true;
+        pauseButton.gameObject.SetActive(false);
+        Time.timeScale = 0;
+        gameMaster.GetComponent<AiGameCore>().gamePaused = true;
+    }
+
+    public void closeMenu() 
+    { 
+        pauseMenu.enabled = false;
+        pauseButton.gameObject.SetActive(true);
+        Time.timeScale = 1;
+        gameMaster.GetComponent<AiGameCore>().gamePaused = false;
+    }
+
+    public async void returnToMain()
+    {
+        GameObject networkingManger = GameObject.Find("NetworkManager");
+
+        // Disconnect from photon if the game is being played online
+        if (networkingManger != null && networkingManger.GetComponent<NetworkingManager>()._runner)
+        {
+            await networkingManger.GetComponent<NetworkingManager>().DisconnectFromPhoton();
+        }
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void openHelpMenu()
+    {
+        pauseMenu.enabled = false;
+        helpMenu.enabled = true;
+    }
+
+    public void closeHelpMenu()
+    {
+        helpMenu.enabled = false;
+        pauseMenu.enabled = true;
+    }
+
+    public void restartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
