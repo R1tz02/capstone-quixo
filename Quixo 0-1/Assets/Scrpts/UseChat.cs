d31809967f7de2910203bcafdd417ff2a3fa13ac
Caleb St Clair <cstclair@harding.edu>
2024-03-29 10:25:34 -0500

    fix: reimplement chat feature over the network
    - still needs the GUI implementation

--- Quixo 0-1/Assets/Scrpts/UseChat.cs
+++ Quixo 0-1/Assets/Scrpts/UseChat.cs
@@ -1,19 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Fusion;
+
 public class UseChat : MonoBehaviour
 {
     public Button openChat;
     public Button closeChat;
     public InputField chat;
     public Image chatCanvas;
-    // Start is called before the first frame update
+
+    // Event for when a new chat message is sent
+    public delegate void ChatUpdated(string message);
+    public static event ChatUpdated OnChatUpdated;
+
     void Start()
     {
         closeChat.gameObject.SetActive(false);
         chatCanvas.enabled = false;
         chat.gameObject.SetActive(false);
+
+        chat.onEndEdit.AddListener(delegate { HandleSubmit(chat.text); });
+        NetworkChat.OnNetworkChatUpdated += UpdateChat;
     }
 
     public void OpenChat()
@@ -30,5 +37,31 @@ public class UseChat : MonoBehaviour
         closeChat.gameObject.SetActive(false);
         chat.gameObject.SetActive(false);
         chatCanvas.enabled = false;
+    }
+
+    public void HandleSubmit(string text)
+    {
+        // Any other input logic should go here. I.E checking for pushing enter etc.
+        // Also need to clear the input field after submitting.
+        if (text != "")
+        {
+            OnChatUpdated?.Invoke(text);
+        }
+    }
+
+    public void UpdateChat(string message, PlayerRef sendingPlayerRef, PlayerRef localPlayerRef)
+    {
+        // @R1tz02: This function will be called on both the client and server when a new chat message is sent.
+        // NetworkingManager.ChatLog contains a list of chatMessage which is an object with a string and the player that sent it.
+        if (sendingPlayerRef == localPlayerRef)
+        {
+            // This is the local player's message
+            Debug.Log("I sent this message: " + message);
+        }
+        else
+        {
+            // This is the remote player's message
+            Debug.Log("They sent this message: " + message);
+        }
     }
 }
