31c72242ad56e31450eb62131b2196372731d63a
Jack Johnson <42984285+R1tz02@users.noreply.github.com>
2024-04-04 00:23:25 -0500

    add intro skip sequence

--- Quixo 0-1/Assets/Scrpts/IntroCutScene.cs
+++ Quixo 0-1/Assets/Scrpts/IntroCutScene.cs
@@ -78,6 +78,6 @@ public class IntroCutScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) { SceneManager.LoadScene(1); }
     }
 }
