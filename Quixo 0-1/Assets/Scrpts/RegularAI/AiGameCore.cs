c52d8de365960da5376023a7354c70438141b456
Jack Johnson <42984285+R1tz02@users.noreply.github.com>
2024-04-09 04:18:39 -0500

    updates scritps

--- Quixo 0-1/Assets/Scrpts/RegularAI/AiGameCore.cs
+++ Quixo 0-1/Assets/Scrpts/RegularAI/AiGameCore.cs
@@ -242,44 +242,44 @@ public class AiGameCore : MonoBehaviour
         if (winType == WinType.vertical)
         {
             SoundFXManage.Instance.PlaySoundFXClip(swordWin, transform, 1f);
-            GameObject sword = Instantiate(swordPrefab, new Vector3(-2800, 140, 0), Quaternion.identity);
+            GameObject sword = Instantiate(swordPrefab, new Vector3(-2800, 135, 0), Quaternion.identity);
             Vector3 scale = sword.transform.localScale;
-            scale.y = 100f;
-            scale.x = 100f;
-            scale.z = 100f;
+            scale.y = 50f;
+            scale.x = 50;
+            scale.z = 50;
             sword.transform.localScale = scale;
             sword.transform.Rotate(90.0f, 0f, 90.0f, Space.Self);
         }
         if (winType == WinType.Leftdiagonal)
         {
             SoundFXManage.Instance.PlaySoundFXClip(axeWin, transform, 1f);
-            GameObject axe = Instantiate(axePrefab, new Vector3(-2800, 140, 45), Quaternion.identity);
+            GameObject axe = Instantiate(axePrefab, new Vector3(-2827, 140, 29), Quaternion.identity);
             Vector3 scale = axe.transform.localScale;
-            scale.y = 80;
-            scale.x = 80;
-            scale.z = 80;
+            scale.y = 90;
+            scale.x = 90;
+            scale.z = 90;
             axe.transform.localScale = scale;
             axe.transform.Rotate(90.0f, 0, 135.0f, Space.Self);
         }
         if (winType == WinType.horizontal)
         {
             SoundFXManage.Instance.PlaySoundFXClip(spearWin, transform, 1f);
-            GameObject spear = Instantiate(spearPrefab, new Vector3(-2850, 140, 45), Quaternion.identity);
+            GameObject spear = Instantiate(spearPrefab, new Vector3(-2851, 140, -79), Quaternion.identity);
             Vector3 scale = spear.transform.localScale;
-            scale.y = 50f;
-            scale.x = 50f;
-            scale.z = 50f;
+            scale.y = 70f;
+            scale.x = 70f;
+            scale.z = 70f;
             spear.transform.localScale = scale;
-            spear.transform.Rotate(0f, 0, 0, Space.Self);
+            spear.transform.Rotate(90f, 0, 0, Space.Self);
         }
         if (winType == WinType.Rightdiagonal)
         {
             SoundFXManage.Instance.PlaySoundFXClip(axeWin, transform, 1f);
-            GameObject axe = Instantiate(axePrefab, new Vector3(-2800, 140, -45), Quaternion.identity);
+            GameObject axe = Instantiate(axePrefab, new Vector3(-2817, 140, -36), Quaternion.identity);
             Vector3 scale = axe.transform.localScale;
-            scale.y = 80;
-            scale.x = 80;
-            scale.z = 80;
+            scale.y = 90;
+            scale.x = 90;
+            scale.z = 90;
             axe.transform.localScale = scale;
             axe.transform.Rotate(-90.0f, 0, 135.0f, Space.Self);
         }
